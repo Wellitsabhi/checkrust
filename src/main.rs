9fa7c0ca1112e0ae421f6682275d116561c973a4
Abhishek Singh <99867024+Wellitsabhi@users.noreply.github.com>
2024-07-11 22:57:39 +0530

    structs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-fn main() {
-    let my_str = String::from("hello world");
-    take_ownership(my_str);
-    // println!("{}", my_fstr);     // This will give error, as it don't have ownership now. 
+struct User{
+    name: String,
+    age: u32,
+    active: bool,
 }
 
-fn take_ownership(some_str: String){
-    println!("{}", some_str);         // now, "some_str" own the data.
+fn main() {
+    let name  = String::from("ethg");
+    let user = User{
+        name: name,
+        age: 30,
+        active: false,
+    };
+    println!("{} is {} years old",user.name, user.age);
 }
